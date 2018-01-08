7c5cdc6f41667e282a0cc6c28c5c709fc0520b2f
Jack Tench <gnjack@gmail.com>
2018-01-08 03:21:17 +0000

    Fix incorrectly labelling days parameter for list in help

--- api/src/messageHandling/HelpHandler.js
+++ api/src/messageHandling/HelpHandler.js
@@ -9,7 +9,7 @@ export default class MembershipHandler {
     var response = `Hey ${message.from.name.split(' ')[0]}, here's what I understand: <ul>
 <li><b>help</b></li>
 <li><b>add <i>&lt;competitor&gt;</i></b></li>
-<li><b>list <i>[&lt;weeks&gt;]</i></b> - leaderboard of competitors and their current skill level, optionally only covering the last <i>&lt;weeks&gt;</i> of matches</li>
+<li><b>list <i>[&lt;days&gt;]</i></b> - leaderboard of competitors and their current skill level, optionally only covering the last <i>&lt;days&gt;</i> of matches</li>
 <li><b><i>&lt;red team&gt;</i> vs <i>&lt;blue team&gt;</i> <i>[&lt;results&gt;]</i></b> - start a new match, optionally recording the results immediately</li>
 <li><b>red <i>&lt;score&gt;</i> blue <i>&lt;score&gt;</i></b> or <b><i>&lt;red score&gt;</i> <i>&lt;blue score&gt;</i></b> - record the results of current match</li>
 <li><b>cancel</b> - cancel current match</li>
